7126e679279891377ab0a65b05f418ee6da04b54
Cafabri <carlosbch021@gmail.com>
2025-06-25 01:11:16 +0100

    Delete StockDataStorage.java

--- time-series-intraday-feeder/src/main/java/es/ulpgc/dacd/timeseries/infrastructure/ports/storage/StockDataStorage.java
+++
@@ -1,9 +0,0 @@
-package es.ulpgc.dacd.timeseries.infrastructure.ports.storage;
-
-import es.ulpgc.dacd.timeseries.domain.model.AlphaVantageEvent;
-
-import java.util.List;
-
-public interface StockDataStorage {
-    void saveOpeningAndClosingEvents(List<AlphaVantageEvent> data, String context);
-}
