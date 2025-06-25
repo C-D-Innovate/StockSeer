5f90bfa146883af46f9ad124f7556b87c775fa2f
Cafabri <carlosbch021@gmail.com>
2025-06-25 01:11:59 +0100

    Delete adapters/util directory

--- time-series-intraday-feeder/src/main/java/es/ulpgc/dacd/timeseries/infrastructure/adapters/util/TimeUtils.java
+++
@@ -1,9 +0,0 @@
-package es.ulpgc.dacd.timeseries.infrastructure.adapters.util;
-
-import java.time.LocalTime;
-
-public class TimeUtils {
-    public static boolean isWithinOneMinuteOf(LocalTime a, LocalTime b) {
-        return Math.abs(a.toSecondOfDay() - b.toSecondOfDay()) <= 60;
-    }
-}
