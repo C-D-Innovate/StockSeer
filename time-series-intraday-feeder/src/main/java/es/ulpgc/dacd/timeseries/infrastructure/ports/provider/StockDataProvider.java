4ea8e6f309c0a0c2a7e31baf05050374f7e6c6dd
Cafabri <carlosbch021@gmail.com>
2025-06-25 01:10:51 +0100

    Delete StockDataProvider.java ya no es necesaria

--- time-series-intraday-feeder/src/main/java/es/ulpgc/dacd/timeseries/infrastructure/ports/provider/StockDataProvider.java
+++
@@ -1,7 +0,0 @@
-package es.ulpgc.dacd.timeseries.infrastructure.ports.provider;
-import es.ulpgc.dacd.timeseries.domain.model.AlphaVantageEvent;
-import java.util.List;
-
-public interface StockDataProvider {
-    List<AlphaVantageEvent> fetch(String symbol);
-}
